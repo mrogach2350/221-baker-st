a1ff9caddb16971a150e2ed794e9031676643433
Michael Rogachevsky <m.rogach@gmail.com>
2018-07-05 16:59:41 -0400

    adds clues component and basic validation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import clues from './data/clues.json';
 
+import Clues from './components/clues.component';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,6 +11,7 @@ class App extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(event) {
@@ -21,6 +24,10 @@ class App extends Component {
     this.setState({clueText: foundClue.clueText});
   }
 
+  handleClick() {
+    window.location = "./data/game-rules.pdf";
+  }
+
   render() {
     return (
       <div className="App">
@@ -28,18 +35,20 @@ class App extends Component {
           <h1 className="App-title">Clues for</h1>
           <br/>
           <h1 className="App-title">221 B Baker St</h1>  
+          <button onClick={this.handleClick}>View Rules</button>
         </header>
-        <br/>
+        {/* <br/>
         <form onSubmit={this.handleSubmit}>
-            <label>
-              Clue #:
-              <input value={this.state.clueNum} onChange={this.handleChange} type="text" name="name" />
-            </label>
-            <input type="submit" value="Deduce" />
-          </form>
+          <label>
+            Clue #:
+            <input value={this.state.clueNum} onChange={this.handleChange} type="text" name="name" />
+          </label>
+          <input type="submit" value="Deduce" />
+        </form>
 
-          <h1>Current Clue</h1>
-          <h3>{this.state.clueText}</h3>
+        <h1>Current Clue</h1>
+        <h3>{this.state.clueText}</h3> */}
+        <Clues></Clues>
       </div>
     );
   }
---
+++ src/components/clues.component.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import clues from '../data/clues.json';
+
+class Clues extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clueNum: '', 
+            clueText: '', 
+            err: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    errorText = ['Invalid Clue Number!', 'Please try again!'];
+
+    handleChange(event) {
+        let query = event.target.value;
+        this.setState({clueNum: query});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        let query = this.state.clueNum;
+        let parsedQuery = parseInt(query);
+        if (!parsedQuery) {
+            this.setState({clueText: this.errorText, err: true});
+        } else {
+            if (parsedQuery > 0 && parsedQuery < 281) {
+                let foundClue = clues.find(clue => clue.clueNumber === parsedQuery);
+                this.setState({clueText: foundClue.clueText, err: false});
+            } else {
+                this.setState({clueText: this.errorText, err: true});
+            }
+        }
+    }
+
+    render() {
+        let displayClue;
+
+        if (this.state.err) {
+            displayClue = <div><h3>{this.state.clueText[0]}</h3><h3>{this.state.clueText[1]}</h3> </div>;
+        } else {
+            displayClue = <h3>{this.state.clueText}</h3>;
+        }
+
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    Clue #:
+                    <input value={this.state.clueNum} onChange={this.handleChange} type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Deduce" />
+                </form>
+
+                <h1>Current Clue</h1>
+                
+                {displayClue}
+            </div>
+        );
+    }
+}
+
+export default Clues;
